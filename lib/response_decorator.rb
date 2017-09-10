c112d5ecdf8e12ae64ff561ef060be252190cef4
Dmitriy Bielorusov <d.belorusov@gmail.com>
2017-09-10 16:43:00 +0300

    Squash Few Helpers

--- lib/response_decorator.rb
+++ lib/response_decorator.rb
@@ -1,13 +1,52 @@
 require "response_decorator/version"
 require "response_decorator/decorator"
-require "response_decorator/paginate_helper"
-require "response_decorator/response_helper"
-require "response_decorator/serializers_helper"
+require "response_decorator/paginate"
 
 module ResponseDecorator
-  module Methods
+  module Helpers
     def decorate(opts = {})
       Decorator.new(opts)
+    end
+
+    def serialize(obj, options = {}, &block)
+      if obj.is_a?(Array)
+        headers 'X-PAGINATE' => paginate_attributes(obj).to_json
+      end
+      Decorator.new(resource: obj, scope: current_ability).serialize
+    end
+
+    def serialize_and_response(object, &block)
+      object = yield(object) if block_given?
+      # POST =>
+      if request.post?
+        if !object.errors.any?
+          status 201
+          serialize object
+        else
+          headers 'X-ERRORS' => object.errors.to_json
+          status 500
+        end
+      # GET =>
+      elsif request.get?
+        if object
+          status 200
+          serialize object
+        else
+          status 404
+        end
+      # PUT/PATCH =>
+      elsif request.put? || request.patch?
+        if !object.errors.any?
+          status 200
+          serialize object
+        else
+          headers 'X-ERRORS' => object.errors.to_json
+          status 404
+        end
+      # DELETE =>
+      elsif request.delete?
+        status(object ? 204 : 404)
+      end
     end
   end
 end
--- lib/response_decorator/paginate.rb
+++ lib/response_decorator/paginate.rb
@@ -1,5 +1,5 @@
 module ResponseDecorator
-  module PaginateHelper
+  module Paginate
     def paginate_attributes(collection)
       {
         limit:  limit,
@@ -8,14 +8,14 @@ module ResponseDecorator
       }
     end
 
-    # Returns {Integer} of items per page based on params[:limit] or
+    # INFO: Returns {Integer} of items per page based on params[:limit] or
     # PER_PAGE const.
     def limit
       limit_params   = params[:limit].to_i
       ( limit_params > 0 ? limit_params : settings.limit_on_page )
     end
 
-    # Returns {Integer} as number of records to skip, taken from params[:offset]
+    # INFO: Returns {Integer} as number of records to skip, taken from params[:offset]
     # or 0 (by default)
     def offset
       params[:offset].to_i
